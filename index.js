a1d8dcd6b205f48c6e5cf02c9513503119603e47
ajay4990245gmail123 <ajay4990245@gmail.com>
2022-10-13 11:45:16 +0530

    file

---
+++ index.js
@@ -0,0 +1,8 @@
+function button() {
+    document.getElementById("new").innerHTML = "Knowing your why, your competition and customers will give you this answer. The customer can be price sensitive but you won't be able to compete on price, so quality, service and choice will allow you to improve your margins."
+    "Look at the local competition and position yourself in the market, this will then allow you to work out what to charge.";
+}
+
+function button1() {
+    document.getElementById("new").innerHTML
+}
